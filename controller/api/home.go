080adeff46c6caeb89460be4a4f31e5052ba5023
luojunjie <luojunjie@inke.cn>
2023-11-30 18:59:12 +0800

    优化

--- controller/api/home.go
+++ controller/api/home.go
@@ -42,6 +42,8 @@ func Movie(c *gin.Context) {
 		{Icon: "https://toolsdar.cn/img/1680313844-Pasted.png", Url: "https://ddys.pro/", Name: "低端影视"},
 		{Icon: "https://toolsdar.cn/img/1676522049-Pasted.png", Url: "https://www.libvio.me/", Name: "LIBVIO影视"},
 		{Icon: "https://toolsdar.cn/img/1673323796-Pasted.png", Url: "https://www.zxzjhd.com/", Name: "在线之家"},
+		{Icon: "https://api.iowen.cn/favicon/cokemv.link.png", Url: "https://cokemv.link/", Name: "COKEMV影视"},
+		{Icon: "https://www.mjtt.fun/template/mojia/asset/img/logo.png", Url: "https://www.mjtt.fun/", Name: "美剧天堂"},
 	}
 	c.JSON(http.StatusOK, gin.H{"movie_sites": movieList})
 }
